9581015d664aaf28e2e36a0d90e30d3eb47bb2d8
William Bonnaventure <informaticien77.fr@gmail.com>
2017-03-27 20:26:01 +0200

    Fix an issue with Sponge ID format

--- src/main/java/com/greatmancode/tools/interfaces/SpongeLoader.java
+++ src/main/java/com/greatmancode/tools/interfaces/SpongeLoader.java
@@ -37,7 +37,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.logging.Level;
 
-@Plugin(id = "GreatmancodeToolsLoader", name = "GreatmancodeToolsLoader", version = "1.0")
+@Plugin(id = "greatmancodetoolsloader", name = "greatmancodetoolsloader", version = "1.0")
 public class SpongeLoader implements Loader {
 
     @Getter
