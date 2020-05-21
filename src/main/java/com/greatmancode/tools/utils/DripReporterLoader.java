b13a9daddd4c5dc40dd2b4211fbc1e73e22aa247
Narimm <benjicharlton@gmail.com>
2020-05-21 10:05:34 +1000

    Update DripReporterHook

--- src/main/java/com/greatmancode/tools/utils/DripReporterLoader.java
+++ src/main/java/com/greatmancode/tools/utils/DripReporterLoader.java
@@ -6,6 +6,7 @@ import com.greatmancode.tools.interfaces.BukkitLoader;
 import com.greatmancode.tools.interfaces.Loader;
 
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 
 /**
  * Created for use for the Add5tar MC Minecraft server
@@ -14,19 +15,27 @@ import org.bukkit.Bukkit;
 public class DripReporterLoader {
 
     private static DripReporterApi api;
-    public static boolean enabled = false;
+    private static boolean enabled = false;
 
     public static boolean hookDripReporterApi(Loader loader) {
         if (loader instanceof BukkitLoader) {
-            if (Bukkit.getPluginManager().isPluginEnabled("DripReporter")) {
-               api = (DripReporterApi) Bukkit.getPluginManager().getPlugin("DripReporter");
-               enabled = true;
-               return enabled;
+            Plugin plugin = Bukkit.getPluginManager().getPlugin("DripReporter");
+            if (plugin != null && plugin.isEnabled()) {
+                if(plugin instanceof DripReporterApi) {
+                    api = (DripReporterApi) plugin;
+                    enabled = true;
+                } else {
+                    plugin.getLogger().warning("GreatMan Code Tools uses a non supported method of hooking DripReporter");
+                    plugin.getLogger().warning("GreatMan Code Tools will not hook DripReporter.");
+
+                }
             }
         }
         return enabled;
     }
-
+    public static boolean isEnabled(){
+        return enabled;
+    }
     public static DripReporterApi getApi(){
         return api;
     }
